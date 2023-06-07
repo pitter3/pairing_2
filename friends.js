fdb8c939fc7ebd095077fd8d838388dfd6dabc1b
Patrick Eitel <patrickeitel12@gmail.com>
2023-06-07 14:55:03 -0700

    Add places

--- friends.js
+++ friends.js
@@ -2,3 +2,6 @@ var arrays = ["Louis", "Sara", "Patrick", "Kasandra", "Kaitlyn"];
 console.log(arrays[2]);
 console.log(arrays.pop());
 console.log(arrays.shift());
+
+var places = ["Paris", "New York", "Bali", "Tulum", "Detriot"];
+console.log(places.slice(2));
